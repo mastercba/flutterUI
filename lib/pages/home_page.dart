1a7772d2b0cb90ae206060b8b71e6edfc0b8dce8
Marco Torrico <marcojtorrico@gmail.com>
2021-05-14 11:57:03 -0400

    6.Widget Text

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -8,7 +8,24 @@ class HomePage extends StatelessWidget {
         title: Text("My flutter app"),
       ),
       body: Center(
-        child: Text("Hello Devs"),
+        child: Text(
+          "Hello \n Developers",
+          textAlign: TextAlign.center,
+          style: TextStyle(
+              //entramos a la clase TextStyle con Ctrl + click
+              color: Colors.red[900],
+              fontSize: 45,
+              letterSpacing: 2,
+              shadows: [
+                //requiere que le pase una lista de shadows
+                Shadow(
+                    color: Colors.grey[400], //MAYUCULA EMPIEZA la Instancia
+                    blurRadius: 3,
+                    offset: Offset(5, 5))
+              ],
+              fontWeight: FontWeight.w300 //.w300   .bold
+              ),
+        ),
       ),
     );
   }
