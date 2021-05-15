2f3fcd7b1e131d6c02fa1ec3235f7d611bcf9869
Marco Torrico <marcojtorrico@gmail.com>
2021-05-15 19:47:32 -0400

    9. El Widget SafeArea + debug banner false

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'pages/home_page.dart';
 
 void main() => runApp(MyApp());
@@ -6,8 +7,11 @@ void main() => runApp(MyApp());
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    SystemChrome.setSystemUIOverlayStyle(SystemUiOverlayStyle.dark);
+
     return MaterialApp(
       home: HomePage(),
+      debugShowCheckedModeBanner: false,
     );
   }
 }
--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -7,26 +7,13 @@ class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text("My flutter app"),
-      ),
-      body: Center(
-        child: CircleContainer(
-          child: SvgPicture.asset(
-            "assets/icons/menu.svg",
-            width: 40,
-            height: 40,
-            color: Colors.black,
-          ),
-          //Icon(
-          //Icons.phone,   //icono del tipo material design
-          //CupertinoIcons.phone,  //icono cupertino
-          //size: 50,
-          //color: Colors.blue,
-          //),
-          width: 100,
-          height: 100,
-          //style: TextStyle(fontSize: 40),
+      backgroundColor: Colors.white,
+      body: SafeArea(
+        top: true,
+        bottom: true,
+        child: Container(
+          child: Text("HOLA"),
+          alignment: Alignment.bottomCenter,
         ),
       ),
     );
